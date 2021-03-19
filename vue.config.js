52acb8a36a8fafeec6da0d150bb43ba7e8784774
iRockE <jmrichards96@gmail.com>
2021-03-18 23:08:02 -0400

    Added Sass preprocessor

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const path = require('path')
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio-vue/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production'? '/portfolio-vue/' : '/',
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, './src/styles/imports.scss'),
+      ]
+    }
+  }
 }
