9c597d4fb061136f123d9877b8dbdb3397696d81
iRockE <jmrichards96@gmail.com>
2021-03-25 15:15:11 -0400

    Fixed deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'? '/portfolio-vue/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/' : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
